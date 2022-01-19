9a94941cceb08928f1befdaee28f7b60a7d13d5d
Larry Simiyu <larrysimiyu@Larrys-MacBook-Pro.local>
2022-01-19 10:47:24 -0800

    image and title string formatting

---
+++ src/formattedStrings/itemImageFormat.js
@@ -0,0 +1,18 @@
+export const itemImages = new Map([
+  ["Redwood", "redwood.jpg"],
+  ["Turf", "turf.jpg"],
+  ["Fountain", "fountain.jpg"],
+  ["Pool", "pool.jpg"],
+  ["Pirate Ship", "pirateShip.jpg"],
+  ["Taj Mahal", "tajMahal.jpg"],
+  ["Pergola", "pergola.jpg"],
+  ["3-5", "3-5.jpg"],
+  ["16+", "16+.jpg"],
+  ["6-15", "6-15.jpg"],
+  ["Pavers", "pavers.jpg"],
+  ["Gravel", "gravel.jpg"],
+  ["Composite", "composite.jpg"],
+  ["Bamboo Shroud", "bambooShroud.jpg"],
+  ["Redwood Fence", "redwoodFence.jpg"],
+  ["Plywood Fence", "plywoodFence.jpg"],
+]);
